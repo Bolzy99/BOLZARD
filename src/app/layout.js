f3ab268c59b59658a1536448cbc46836d21e08bf
Bolzy99 <yash.super50@gmail.com>
2025-09-19 22:28:24 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,8 @@ import { Instrument_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import ChatbotWidget from "./components/ChatbotWidget"; // 1. Import the widget
+import ChatbotWidget from "./components/ChatbotWidget";
+import ParticleBackground from "./components/ParticleBackground"; // ADDED
 
 const instrumentSans = Instrument_Sans({
   subsets: ["latin"],
@@ -19,13 +20,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${instrumentSans.variable} font-sans bg-black text-white`}>
+      {/* Make body relative so the background canvas can anchor and sit behind all content */}
+      <body className={`${instrumentSans.variable} font-sans bg-black text-white relative`}>
+        {/* Render particles once, globally */}
+        <ParticleBackground />
         <Navbar />
         <main>
           {children}
         </main>
         <Footer />
-        <ChatbotWidget /> {/* 2. Add the widget here */}
+        <ChatbotWidget />
       </body>
     </html>
   );
