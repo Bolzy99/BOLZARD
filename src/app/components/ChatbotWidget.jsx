8d2d632a557aaa512d0e2fd0e1494c6f234cf3af
Bolzy99 <yash.super50@gmail.com>
2025-10-20 20:46:41 +0530

    Update ChatbotWidget.jsx

--- src/app/components/ChatbotWidget.jsx
+++ src/app/components/ChatbotWidget.jsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 
 
 // --- CONFIGURATION ---
-const WEBHOOK_URL = "https://n8n.srv1018795.hstgr.cloud/webhook/bolzard"; 
+const WEBHOOK_URL = "https://n8n.srv1075357.hstgr.cloud/webhook/bolzard"; 
 const BOT_NAME = "BOLZARD AI";
 const INITIAL_MESSAGE = {
   id: "init",
